f74bb8f0c471f0878eb43da80a1ef9b8bf3bd0af
Tarik Cekirge <tcekirge@gmail.com>
2022-12-24 13:15:11 +0300

    harf-eslestirme

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# HarfEslestirme
+# Angular Harf Eşleştirme
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.0.2.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
-
+import { faker } from '@faker-js/faker';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'harf-eslestirme';
+  randomParagraph = faker.lorem.paragraph();
+  enteredText:String="";
+
+  getInputValue(value:String){
+    this.enteredText =value;
+    console.log(this.randomParagraph.split(''))
+  }
+  compare(randomLetter:String,enteredLetter:String ){
+    if(!enteredLetter){
+      return 'pending';
+    }
+    return randomLetter === enteredLetter ? 'correct':'incorrect'
+  }
 }
+
+
